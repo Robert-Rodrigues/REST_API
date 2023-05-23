dfd09da3f9490676924bc30925959052fb65b469
Thiago Jefferson Cordeiro Mendes de lira <tjlira07@gmail.com>
2023-05-23 00:40:16 -0300

    class comments

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const Comment = require('../models/comment');
 
 // Criar um novo usuário
 router.post('/', (req, res) => {
@@ -92,5 +93,88 @@ router.put('/:id/restore', (req, res) => {
     })
     .catch((err) => res.status(500).json(err));
 });
+
+// Criar um novo comentário
+router.post('/:userId/comments', (req, res) => {
+  const { userId } = req.params;
+  const { text } = req.body;
+  
+  Comment.create({
+    userId,
+    text,
+  })
+    .then((comment) => {
+      res.json(comment);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+// Obter todos os comentários de um usuário
+router.get('/:userId/comments', (req, res) => {
+  const { userId } = req.params;
+  
+  Comment.findAll({
+    where: {
+      userId,
+    },
+  })
+    .then((comments) => res.json(comments))
+    .catch((err) => res.status(500).json(err));
+});
+
+// Obter um comentário por ID
+router.get('/:userId/comments/:commentId', (req, res) => {
+  const { userId, commentId } = req.params;
+  
+  Comment.findOne({
+    where: {
+      userId,
+      id: commentId,
+    },
+  })
+    .then((comment) => {
+      if (!comment) {
+        return res.status(404).json({ message: 'Comment not found' });
+      }
+      res.json(comment);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+// Atualizar um comentário por ID
+router.put('/:userId/comments/:commentId', (req, res) => {
+  const { userId, commentId } = req.params;
+  const { text } = req.body;
+  
+  Comment.update(
+    { text },
+    {
+      where: {
+        userId,
+        id: commentId,
+      },
+    },
+  )
+    .then(() => {
+      res.json({ message: 'Comment updated successfully' });
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+// Deletar um comentário por ID
+router.delete('/:userId/comments/:commentId', (req, res) => {
+  const { userId, commentId } = req.params;
+  
+  Comment.destroy({
+    where: {
+      userId,
+      id: commentId,
+    },
+  })
+    .then(() => {
+      res.json({ message: 'Comment deleted successfully' });
+    })
+    .catch((err) => res.status(500).json(err));
+  });
 
 module.exports = router;
